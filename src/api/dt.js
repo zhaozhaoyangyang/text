a43adb6ccb682b0119405e63c5bf3e61f2773f2a
花海 <746493056@qq.com>
2021-06-05 21:55:17 +0800

    登录成功

--- src/api/dt.js
+++ src/api/dt.js
@@ -20,7 +20,6 @@ export const reqClassify = (data) => get("/dj/recommend/type", data);
 // 电台节目
 export const reqDetail = (id) => get("/dj/program" + id);
 //节目详情 获取电台节目后用id调歌曲
-export const reqDetail = (id) => get("/dj/program/detail" + id);
-//今日优选
-export const reqPerfered = () => get("/dj/today/perfered");
-
+// export const reqDetail = (id) => get("/dj/program/detail" + id);
+// //今日优选
+// export const reqPerfered = () => get("/dj/today/perfered");
