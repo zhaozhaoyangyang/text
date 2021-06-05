dd9595066c1bdd864aed52314af56701c9a8fc28
花海 <746493056@qq.com>
2021-06-05 22:02:41 +0800

    登录成功

--- src/components/footer.vue
+++ src/components/footer.vue
@@ -1,20 +1,11 @@
 <template>
   <div class="">
-<<<<<<< HEAD
     <van-tabbar route>
       <van-tabbar-item to="/sy" icon="home-o">首页</van-tabbar-item>
       <van-tabbar-item to="/fl" icon="apps-o">分类</van-tabbar-item>
       <van-tabbar-item to="/play" icon="play-circle-o">播放</van-tabbar-item>
       <van-tabbar-item to="/radio" icon="service-o">电台</van-tabbar-item>
       <van-tabbar-item to="/mine" icon="manager-o">我的</van-tabbar-item>
-=======
-    <van-tabbar  route>
-      <van-tabbar-item to='/sy' icon="home-o">首页</van-tabbar-item>
-      <van-tabbar-item to='/fl' icon="search">分类</van-tabbar-item>
-      <van-tabbar-item to='/play' icon="friends-o">播放</van-tabbar-item>
-      <van-tabbar-item to='/radio' icon="setting-o">电台</van-tabbar-item>
-      <van-tabbar-item to='/mine' icon="manager">我的</van-tabbar-item>
->>>>>>> cb754639178660f8f45af79df0ad0645be87af68
     </van-tabbar>
   </div>
 </template>
--- src/main.js
+++ src/main.js
@@ -1,15 +1,3 @@
-<<<<<<< HEAD
-import Vue from 'vue'
-import App from './App.vue'
-import Vant from 'vant'
-import Vuex from 'vuex'
-import axios from 'axios'
-import router from './router'
-import 'vant/lib/index.css';
-import ElementUI from 'element-ui';
-import 'element-ui/lib/theme-chalk/index.css';
-Vue.use(Vant)
-=======
 import Vue from "vue";
 import App from "./App.vue";
 import Vant from "vant";
@@ -20,20 +8,13 @@ import { Icon } from "vant";
 import "vant/lib/index.css";
 Vue.use(Vant);
 Vue.use(Icon);
->>>>>>> e2c501b8a659f5af944766cbfc6d716b41d72726
 
 Vue.config.productionTip = false;
 
 new Vue({
-<<<<<<< HEAD
-  render: h => h(App),
-  Vant,Vuex,axios,router,ElementUI
-}).$mount('#app')
-=======
   render: (h) => h(App),
   Vant,
   Vuex,
   axios,
   router,
 }).$mount("#app");
->>>>>>> e2c501b8a659f5af944766cbfc6d716b41d72726
